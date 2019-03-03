7e6d5ce24982d0b80d4a2feeea55714cc4da073e
Lawrence Wagerfield <lawrence@wagerfield.com>
2019-03-03 22:07:29 +0000

    Reformat notebooks to use same width as GitHub preview

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ docker run \
        --rm \
        -p 8888:8888 \
        -v ${DIR}/notebooks:/home/jovyan/work \
+       -v ${DIR}/customization:/home/jovyan/.jupyter/custom \
        jupyter/tensorflow-notebook:45b8529a6bfc \
        start-notebook.sh \
        --NotebookApp.token=
