7d79381e8190bb80845bcca4c286c47278146fcc
Lawrence Wagerfield <lawrence@wagerfield.com>
2019-03-03 21:25:42 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Notebooks
+
+This repository contains several experiements involving Machine Learning using Jupyter (Python) notebooks.
+
+Python is not my first language... please forgive any heavy-handedness.
+
+## Prerequisites
+
+-   Docker
+
+## Running
+
+```bash
+./run.sh
+```
---
+++ run.sh
@@ -0,0 +1,9 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+docker run \
+       --rm \
+       -p 8888:8888 \
+       -v ${DIR}/notebooks:/home/jovyan/work \
+       jupyter/tensorflow-notebook:45b8529a6bfc \
+       start-notebook.sh \
+       --NotebookApp.token=
